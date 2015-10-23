9d56a841d5781c642549a618519ea90e04129452
Yemi Kehinde <y.kehinde@hotmail.com>
2015-10-23 16:23:38 +0100

    A lot more updates to gulp file including image min, grunticon (yes!!), some added packages

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,15 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
     path = require('path'),
     uglify = require('gulp-uglify'),
+    newer = require('gulp-newer'),
     imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cached'),
+    mozjpeg = require('imagemin-mozjpeg'),
     pngquant = require('imagemin-pngquant'),
     gulpicon = require("gulpicon/tasks/gulpicon"),
     browserSync = require('browser-sync').create(),
     sourcemaps = require('gulp-sourcemaps'),
+    clean = require('gulp-clean'),
     concat = require('gulp-concat');
 
 var basePaths = {
@@ -29,8 +33,8 @@ var basePaths = {
             dest: basePaths.dest + 'img/icons/'
         },
         js: {
-            src: basePaths.src + 'js/',
-            dest: basePaths.dest + 'js/'
+            src: basePaths.src + ['js/'],
+            dest: basePaths.dest + ['js/']
         },
         styles: {
             src: basePaths.src + 'less/',
@@ -38,12 +42,11 @@ var basePaths = {
         }
     };
 
-
 // Gulpicon
 // https://github.com/filamentgroup/gulpicon
 
 // grab the config, tack on the output destination
-var config = require("./gulp-configs/config.js");
+var config = require("./gulp-configs/grunticon/config.js");
 config.dest = paths.icons.dest;
 console.log("this");
 
@@ -51,50 +54,67 @@ console.log("this");
 var files = glob.sync(paths.icons.src + '*.svg');
 
 // set up the gulp task
-gulp.task("icons", gulpicon(files, config));
+gulp.task('icons', ['imagemin', 'grunticon']);
 
 // Gulp Imagemin
 gulp.task('imagemin', function() {
-    return gulp.src(paths.images.src + '**/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{
-                removeViewBox: false
-            }],
-            use: [pngquant()]
-        }))
-        .pipe($.cache($.imagemin({
+    return gulp.src(paths.images.src + '**/*.{png,jpg,gif}')
+        .pipe(clean())
+        .pipe(newer(paths.images.dest))
+        .pipe(cache(imagemin({
             optimizationLevel: 3,
             progressive: true,
-            interlaced: true
+            interlaced: true,
+            svgoPlugins: [
+	{ removeXMLProcInst: false }, // keep XML declaration to avoid errors creating PNG on Win 7
+                    { removeViewBox: false },
+                    { removeUselessStrokeAndFill: false },
+                    { cleanupIDs : true },
+                    { removeUnknownsAndDefaults : true}
+            ],
+            use: [mozjpeg(), pngquant()]
         })))
         .pipe(gulp.dest(paths.images.dest))
         .pipe(browserSync.stream());
+        _initGrunticon();
+});
+
+gulp.task('grunticon', function(){
+
+    var svgFiles = glob.sync(paths.icons.src + '**/*.svg');
+    var grunticon = gulpicon(svgFiles, config);
+
+    grunticon();
+})
+
+// Copy files over
+gulp.task('copy-files', function() {
+   gulp.src('./assets/src/js/libs/*.js')
+   .pipe(uglify())
+   .pipe(gulp.dest('./assets/dist/js/libs'));
 });
 
 // Gulp concat and uglify js
 gulp.task('js', function() {
-    gulp.src(paths.js.src)
+	gulp.src('./assets/src/js/libs/*.js')
+	   .pipe(uglify())	   
+       .pipe(gulp.dest('./assets/dist/js/libs'));
+    return gulp.src([
+    	'./node_modules/jquery/src',
+    	paths.js.src + 'helpers/console.js',
+    	paths.js.src + '*.js'
+    	])
         .pipe(sourcemaps.init())
+        .pipe(uglify('script.min.js', 
+        {
+        	compress: true,
+        	outSourceMap: true
+        	// beautify: true
+        }))
         .pipe(concat('script.min.js'))
-        .pipe($.uglify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.js.dest))
-        .pipe(browserSync.stream());
-});
-
-// BrowserSync - local server
-gulp.task('serve', ['less'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-
-    gulp.watch(paths.styles.src + '**/*', ['less']).on('change', browserSync.reload);
-    gulp.watch(paths.js.src + '**/*', ['js']).on('change', browserSync.reload);
-    gulp.watch(paths.images.src + '**/*', ['icons', 'imagemin']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+        .pipe(gulp.dest(paths.js.dest));
+        // .pipe(browserSync.stream());
 });
 
 // Less task to compile and minify CSS
@@ -107,6 +127,21 @@ gulp.task('less', function() {
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.styles.dest))
         .pipe(browserSync.stream());
+});
+
+// BrowserSync - local server
+gulp.task('serve', ['less', 'js', 'icons', 'copy-files'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch(paths.styles.src + '**/*', ['less']).on('change', browserSync.reload);
+    gulp.watch(paths.js.src + '**/*', ['js']).on('change', browserSync.reload);
+    gulp.watch(paths.images.src + '**/*', ['imagemin']);
+    gulp.watch(paths.icons.src + '**/*', ['icons']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
